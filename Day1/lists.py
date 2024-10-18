e931b58ac5c280ba07072ee7d9837de99f2dc96f
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-10-18 14:56:20 +0200

    Prepared day1 structure, gitignore and licence added

---
+++ Day1/lists.py
@@ -0,0 +1,14 @@
+import operator
+import random
+import os
+import re
+from collections import defaultdict
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Pyton Kurs')
+
