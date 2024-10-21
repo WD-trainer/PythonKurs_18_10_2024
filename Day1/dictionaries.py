df230f530e22df2f3d39b1b4e039b9dc57319196
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-10-21 11:43:55 +0200

    Dicts task 2

--- Day1/dictionaries.py
+++ Day1/dictionaries.py
@@ -50,5 +50,46 @@ if __name__ == '__main__':
 
     # wczytaj dane do słownika w ten sposób by pierwsza kolumna stanowila klucze a
     # druga przypisane do nich wartości. Przeiteruj po słowniku i wypisz klucze oraz przypisane do nich wartości
+    ustawienia = {}
 
+    with open("ustawienia.txt", "r") as file:
+        for line in file:
+            line = line.strip().split(';')
+            if len(line) == 2:
+                key, value = line
+                ustawienia[key] = value
 
+    for key, value in ustawienia.items():
+        print(f'Klucz: {key}, wartosc: {value}')
+
+    # Dlaczego tak?
+    # def search_dict_by_value(dictionary: dict, searched_value: str) -> list[str]:
+    #     found_elements = [value for k, v in dictionary.items() if v == searched_value]
+    #     return found_elements
+
+    studenci = {
+        1001: {'imie': 'Jan', 'nazwisko': 'Kowalski', 'wiek': 21, 'oceny': [4, 3, 5, 4]},
+        1002: {'imie': 'Anna', 'nazwisko': 'Nowak', 'wiek': 22, 'oceny': [5, 5, 4, 5]},
+        1003: {'imie': 'Marek', 'nazwisko': 'Zielinski', 'wiek': 23, 'oceny': [3, 4, 2, 3]},
+        1004: {'imie': 'Zofia', 'nazwisko': 'Wiśniewska', 'wiek': 20, 'oceny': [4, 4, 4, 4]},
+        1005: {'imie': 'Krzysztof', 'nazwisko': 'Wojcik', 'wiek': 24, 'oceny': [2, 3, 2, 3]}
+    }
+
+    studenci[1006] = {'imie': 'Krzysztof2', 'nazwisko': 'Wojcik', 'wiek': 26, 'oceny': [2, 3, 2, 3]}
+
+    list_of_names = [studenci[id]['imie'] for id in studenci]
+    print(list_of_names)
+
+    #  Oblicz średnią ocenę dla każdego studenta i dodaj ją do ich słownika
+    for id in studenci:
+        oceny = studenci[id]['oceny']
+        srednia = sum(oceny) / len(oceny)
+        studenci[id]['srednia_ocen'] = srednia
+
+    print(studenci)
+
+    nazwiska = ['Kowalski', 'Nowak', 'Zielinski', 'Wiśniewska', 'Wojcik']
+    dlugosci_nazwisk = {nazwisko : len(nazwisko) for nazwisko in nazwiska if len(nazwisko) > 6}
+    print(dlugosci_nazwisk)
+
+    #  Znajdź studentów, którzy mają średnią ocenę powyżej 4.0, stworz słownik id: słownik 
