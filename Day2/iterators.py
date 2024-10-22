c90174f8e8dc501c897baa882455a3c4b43ae4fe
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-10-22 16:01:28 +0200

    Iterator finish

--- Day2/iterators.py
+++ Day2/iterators.py
@@ -2,6 +2,12 @@ from collections.abc import Iterator
 
 from classes_and_objects import Zawodnik
 
+print("*" * 50)
+print("*" * 50)
+print("*" * 50)
+print("*" * 50)
+print("*" * 50)
+
 class IncrementIterator:
     def __init__(self, n):
         self.n = n
@@ -20,7 +26,7 @@ class IncrementIterator:
 for e in IncrementIterator(10):
     print(e)
 
-
+print("*" * 50)
 
 
 class MiesiaceIterator:
@@ -60,6 +66,8 @@ for i in range(11):
     print(next(miesiace_iterator))
 
 
+print("*" * 50)
+
 # uzupełnic klase lista zwodnikow o metody __iter__ oraz __next__
 class ListaZawodnikow(Iterator):
     def __init__(self, path:str):
@@ -73,6 +81,13 @@ class ListaZawodnikow(Iterator):
         self.index = 0
 
 
+    def __next__(self):
+        if self.index == len(self.zawodnicy):
+            raise StopIteration
+        self.index += 1
+        return self.zawodnicy[self.index - 1]
+
+
 
 
 nasza_lista = ListaZawodnikow("dane.txt")
@@ -80,3 +95,72 @@ zawodnik = next(nasza_lista)
 
 for z in nasza_lista:
     print(z)
+
+
+print("*" * 50)
+
+########################## Kiedy __iter__ nie zwraca self
+class MyCollection:
+    def __init__(self, items):
+        self.items = items
+
+    def __iter__(self):
+        return MyCollectionIterator(self.items)
+
+
+class MyCollectionIterator:
+    def __init__(self, items):
+        self.items = items
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.items):
+            raise StopIteration
+        item = self.items[self.index]
+        self.index += 1
+        return item
+
+
+# Tworzymy kolekcję
+collection = MyCollection([1, 2, 3])
+
+# Iterujemy po kolekcji
+for item in collection:
+    print(item)
+
+# Iterujemy po kolekcji kolejny raz
+for item in collection:
+    print(item)
+
+
+print("*" * 50)
+
+
+
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+
+# Definiujemy niestandardowy dataset
+class MyDataset(Dataset):
+    def __init__(self):
+        self.data = torch.arange(10)  # Tworzymy proste dane od 0 do 9
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+# Tworzymy dataset
+dataset = MyDataset()
+
+# Używamy DataLoader, który tworzy iterator
+dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+
+# Iterujemy po DataLoader (wykorzystując iterator wbudowany w DataLoader)
+for batch in dataloader:
+    print(batch)
