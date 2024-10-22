8047cb4349dfb52b3a38d21c5826d199b12ace85
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-10-22 08:57:36 +0200

    Added structure to day 2

---
+++ Day2/classes_and_objects.py
@@ -0,0 +1,7 @@
+from timeit import default_timer as timer
+import time
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
---
+++ Day2/decorators.py
@@ -0,0 +1,13 @@
+import operator
+import random
+import os
+import re
+from collections import defaultdict
+
+from datetime import datetime
+import time
+import functools
+import logging
+
+
+
