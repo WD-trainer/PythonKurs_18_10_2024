10b11f9fefed3c34f58c0cff879214b2faf275e9
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-10-23 09:32:26 +0200

    Generators task 2, structure

--- Day3/generators.py
+++ Day3/generators.py
@@ -1,4 +1,5 @@
 import time
+import itertools
 
 
 def elements():
@@ -59,4 +60,40 @@ if __name__ == '__main__':
 
     #  Stworz generator ktory bedzie przyjmowal przez parametr ilosc elementow a nastepnie zwracal elementy o tresci
     #  'element o indeksie x'( gdzie x bedzie numerem podawanego elementu) czekajac 1 sekunde przed zwrotem kazdego elementu.
+    def generator_elementow(ilosc: int):
+        for i in range(ilosc):
+            # time.sleep(1)
+            yield f'element o indeksie {i}'
 
+
+    genrator = generator_elementow(3)
+    print(next(genrator))
+    next(genrator)
+    print(next(genrator))
+
+    # next(genrator)
+    nowy_genrator = generator_elementow(10)
+
+
+
+    def is_prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+
+    # napisz generator ktory bedzie zwracan nieskonczenie wiele liczb pierwszych
+    
+
+
+
+
+    generator_of_primes = prime_numbers()
+
+    for i in range(20000000):
+        if i > 1000:
+            break
+        print(next(generator_of_primes))
